5a1174ba73f6342353f90bf4cd572179acda4b00
ultravioletcatastrophe <34731419+ultravioletcatastrophe@users.noreply.github.com>
2025-09-17 00:06:32 -0500

    Delete localbuild directory

--- localbuild/Build/Intermediates.noindex/Spaceman.build/Debug/Spaceman.build/Script-E8EBF08E25760A8F001BA74F.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Type a script or drag a script file from your workspace to insert its path.
-"${BUILT_PRODUCTS_DIR}/LaunchAtLogin_LaunchAtLogin.bundle/Contents/Resources/copy-helper-swiftpm.sh"
-
