d3d7d8f54c458ced12bf8848540eb2925d0fcd43
waylon.wang <waylon@waylon.wang>
2025-09-20 15:19:28 +0800

    feat: Added configurable fill order for Dual Row layout (contribution Waylon Wang)

---
+++ Spaceman/Model/DualRowFillOrder.swift
@@ -0,0 +1,13 @@
+//
+//  DualRowFillOrder.swift
+//  Spaceman
+//
+//  Created by Waylon Wang on 20/09/2025.
+//
+
+import Foundation
+
+// Visual ordering of spaces in dual-row layout
+enum DualRowFillOrder: Int {
+    case byColumn, byRow
+}
