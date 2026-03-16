9bc2904bebdae1f716f0a43a17e6dbcfaa555782
René Uittenbogaard <ruittenb@gmail.com>
2026-03-16 11:30:17 +0100

    Support osascript -e 'tell application "Spaceman" to restore preferences'

--- Spaceman/Helpers/OpenPreferencesCommand.swift
+++ Spaceman/Helpers/OpenPreferencesCommand.swift
@@ -1,5 +1,5 @@
 //
-//  PreferencesCommand.swift
+//  OpenPreferencesCommand.swift
 //  Spaceman
 //
 //  Created by Claude Code on 21/09/2025.
@@ -8,10 +8,8 @@
 import Foundation
 import Cocoa
 
-class PreferencesCommand: NSScriptCommand {
+class OpenPreferencesCommand: NSScriptCommand {
     override func performDefaultImplementation() -> Any? {
-        print("PreferencesCommand: performDefaultImplementation called")
-
         // Use the same approach as RefreshCommand - post a notification
         DispatchQueue.main.async {
             NotificationCenter.default.post(name: NSNotification.Name(rawValue: "OpenPreferences"), object: nil)
