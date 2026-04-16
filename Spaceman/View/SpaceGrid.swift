7f7c7e21c68dc218d9b93893c669c70e3bc87f83
René Uittenbogaard <ruittenb@gmail.com>
2026-04-16 13:31:24 +0200

    Allow color to be changed from within Quick Rename View
    Also remove dead code

--- Spaceman/View/SpaceGrid.swift
+++ Spaceman/View/SpaceGrid.swift
@@ -1,5 +1,5 @@
 //
-//  SpaceGridPopover.swift
+//  SpaceGrid.swift
 //  Spaceman
 //
 //  Created by René Uittenbogaard on 2026-04-03.
@@ -7,49 +7,6 @@
 //
 
 import SwiftUI
-
-struct SpaceGridPopover: View {
-    let spaces: [Space]
-    var onSwitch: (Int) -> Void
-
-    @AppStorage("gridColumns") private var gridColumns: Int = 3
-
-    var body: some View {
-        VStack(spacing: 8) {
-            let columns = Array(repeating: GridItem(.flexible(), spacing: 4),
-                                count: max(1, min(gridColumns, spaces.count)))
-            LazyVGrid(columns: columns, spacing: 4) {
-                ForEach(Array(spaces.enumerated()), id: \.element.spaceID) { _, space in
-                    SpaceCellView(space: space)
-                        .onTapGesture {
-                            if !space.isCurrentSpace {
-                                let index = Int(space.spaceByDesktopID) ?? Space.unswitchableIndex
-                                onSwitch(index)
-                            }
-                        }
-                }
-            }
-
-            Divider()
-
-            HStack {
-                Text("Columns")
-                    .font(.caption)
-                    .foregroundColor(.secondary)
-                Slider(value: Binding(
-                    get: { Double(gridColumns) },
-                    set: { gridColumns = max(1, Int($0)) }
-                ), in: 1...Double(max(2, spaces.count)), step: 1)
-                Text("\(gridColumns)")
-                    .font(.caption)
-                    .monospacedDigit()
-                    .frame(width: 16, alignment: .trailing)
-            }
-        }
-        .padding(12)
-        .frame(minWidth: 160)
-    }
-}
 
 struct SpaceGridMenuView: View {
     let spaces: [Space]
